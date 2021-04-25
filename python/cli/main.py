3c8e3ce47a3f61c4b6d461bb509efa678d27345e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-25 10:22:16 +0430

    Created

---
+++ python/cli/main.py
@@ -0,0 +1,36 @@
+row = 0
+tasks = {}
+
+print("Welcome to TODO app\n")
+
+while True:
+    work = str(input("[A]dd , [C]heck , [L]ist , [E]xit >>> "))
+
+    if work.lower() == "a" :
+        print("\nNow add your job as this template:")
+        print("Name Text")
+        
+        add = str(input("\nOk, Go on >>> ")).split(" ")
+        
+        row = row + 1
+
+        tasks.update({ row : [add[0] , add[1] , "ny"]})
+
+        print("Done !")
+
+    elif work.lower() == "c" :
+        check = int(input("\nWrite row of your job >>> "))
+
+        tasks.pop(check)
+
+        print(tasks)
+
+    elif work.lower() == "l" :
+        print(tasks)
+
+    elif work.lower() == "e" :
+        print("Ok, Bye !")
+        quit()
+
+    else :
+        continue
